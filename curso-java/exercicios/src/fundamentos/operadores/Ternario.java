2adc1b30277a7949d393578fd0e26ff89c10b0a2
mjordan <mjordan@CWBENGWL1CAGY0.scopus.com.br>
2020-03-09 15:47:30 -0300

    fundamentos completo

--- curso-java/exercicios/src/fundamentos/operadores/Ternario.java
+++ curso-java/exercicios/src/fundamentos/operadores/Ternario.java
@@ -4,10 +4,17 @@ public class Ternario {
 
 	public static void main(String[] args) {
 		
-		double media = 4.6;
+		double media = 7.6;
 		String resultadoParcial = media >= 5.0 ? "em recuperação" : "reprovado";
 		String resultadoFinal = media >= 7.0 ? "aprovado." : resultadoParcial;
-		
 		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = true;
+		boolean passouPorMedia = (nota >= 7);
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim." : "Não.";
+		
+		System.out.println("Tem desconto? " + resultado);			
 	}
 }
